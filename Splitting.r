86eedac1557638feaf51ca9b6bd2aa3472fd27ce
Alex Kelly <lexkel@hotmail.com>
2018-05-30 12:09:44 +1000

    Updated to split using st_difference as well as st_intersection.

--- Splitting.r
+++ Splitting.r
@@ -47,11 +47,11 @@
   #----------------------------------------------------------------------------------
   
     # Target regions to be split - e.g. destination zone
-    split.target <- st_read("./Custom/Townships.shp")
+    split.target <- st_read("./SA4/SA4_2016_AUST.shp")
     split.target <- st_buffer(split.target, 0)
     
     # Regions to be used to split the target layer - e.g. mornington peninsula townships
-    split.by <- st_read("./DZN/DZN_2016_AUST.shp")
+    split.by <- st_read("./LGA/LGA_2016_AUST.shp")
     split.by <- st_buffer(split.by, 0)
   
   #----------------------------------------------------------------------------------
@@ -61,38 +61,54 @@
     st_crs(split.target) == st_crs(split.by)  
   
   #----------------------------------------------------------------------------------
-  # Split
+  # Filter (if appropriate)
+  #----------------------------------------------------------------------------------   
+    
+    split.target <- split.target %>% filter(SA4_NAME16 == "Geelong")
+    
+    split.by <- split.by %>% filter(LGA_NAME16 == "Greater Geelong (C)")
+    
+  #----------------------------------------------------------------------------------
+  # Split by difference of intersection
   #----------------------------------------------------------------------------------  
   
-    # Find intersections and calculate overlapping area (to use as a threshold)
-    int <- st_intersection(split.target, split.by)
-    int$overlap.pct <- round(as.numeric(st_area(int)) / as.numeric(st_area(split.by)) * 100, 2) 
-    int <- int %>% filter(overlap.pct >= 1.0) # exclude overlaps of less than 1 pct
+    #~~~~~~~~~~~~~~~~~
+    # 1. Intersection
+    #~~~~~~~~~~~~~~~~~
     
-    # Centroids for adding labels
-    centroids <- st_centroid(int)
-    coords <- st_coordinates(centroids)
+      # # Find intersections and calculate overlapping area (to use as a threshold)
+      # int <- st_intersection(split.target, split.by)
+      # int$overlap.pct <- round(as.numeric(st_area(int)) / as.numeric(st_area(split.by)) * 100, 2) 
+      # int <- int %>% filter(overlap.pct >= 1.0) # exclude overlaps of less than 1 pct
+      # 
+      # # Centroids for adding labels
+      # centroids <- st_centroid(int)
+      # coords <- st_coordinates(centroids)
+      # 
+      # # Filter list 
+      # SA2.list <- c("Point Nepean", "Flinders", "Rosebud - McCrae",
+      #               "Dromana", "Mount Martha", "Mornington", "Mount Eliza",
+      #               "Somerville", "Hastings - Somers")
+      # 
+      # # Base r plot
+      # plot(st_geometry(split.by %>% filter(SA2_NAME16 %in% SA2.list)), border = "red", 
+      #     col = adjustcolor("grey", alpha.f = 0.1), lwd = 1)
+      # plot(st_geometry(split.target), add = TRUE, border = "black", lwd = 2)
+      # title(main = "Mornington Peninsula townships",
+      #       sub = "Red lines indicate where destinations zones cut through the townships")
+      # 
+      # # Save
+      # st_write(dif, "Geelong_SA4_outside_Geelong_LGA.shp")
     
-    # Filter list 
-    SA2.list <- c("Point Nepean", "Flinders", "Rosebud - McCrae",
-                  "Dromana", "Mount Martha", "Mornington", "Mount Eliza",
-                  "Somerville", "Hastings - Somers")
-    
-    # Base r plot (faster)
-    plot(st_geometry(split.by %>% filter(SA2_NAME16 %in% SA2.list)), border = "red", 
-        col = adjustcolor("grey", alpha.f = 0.1), lwd = 1)
-    plot(st_geometry(split.target), add = TRUE, border = "black", lwd = 2)
-    #text(coords[,1], coords[,2], labels = centroids$Zone.Name, pch = 20)
-    title(main = "Mornington Peninsula townships",
-          sub = "Red lines indicate where destinations zones cut through the townships")
-    
-    # Ggplot (slower)
-    ggplot() +
-      geom_sf(data = split.by %>% filter(SA2_NAME16 %in% SA2.list), colour = "red", fill = "grey95")  +
-      geom_sf(data = split.target, colour = "black", size = 1, fill = "NA") +
-      #geom_text_repel(data = coords %>% as.data.frame(),
-      #                aes(x = X, y = Y, label = centroids$Zone.Name), box.padding = 0.25, size = 2.5) +
-      labs(title = "Mornington Peninsula townships",
-           subtitle = "Red lines indicate where destinations zones cut through the townships") +
-      coord_sf()
-
+    #~~~~~~~~~~~~~~~
+    # 2. Difference
+    #~~~~~~~~~~~~~~~
+      
+      # Find differnce
+      dif <- st_difference(split.target, split.by)
+  
+      # Base r plot
+      plot(st_geometry(dif))
+      
+      # Save
+      st_write(dif, "Geelong_SA4_outside_Geelong_LGA.shp")
