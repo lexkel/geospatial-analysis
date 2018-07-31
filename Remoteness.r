dfc915b3e8de920e49ab1c6c79dcd97d0e30af60
Alex Kelly <lexkel@hotmail.com>
2018-07-31 13:59:19 +1000

    Remoteness areas by LGA

--- Remoteness.r
+++ Remoteness.r
@@ -13,6 +13,8 @@
   setwd("C:/Users/a.kelly/Documents/Projects/Geospatial analysis")
   
   # Shapefile with regions to join
+  lga <- st_read("./LGA/LGA_2016_AUST.shp")
+  
   areas <- st_read("./RA/RA_2016_AUST.shp") %>% 
             filter(RA_NAME16 %in% c("Inner Regional Australia",
                                     "Outer Regional Australia",
@@ -29,7 +31,7 @@
 
   x <- st_intersection(lga, areas)
   x <- x %>% 
-        st_set_geometry(NULL) %>% 
+        #st_set_geometry(NULL) %>% 
         mutate(Remoteness = case_when(RA_NAME16 == "Very Remote Australia" ~ 5,
                                       RA_NAME16 == "Remote Australia" ~ 4,
                                       RA_NAME16 == "Outer Regional Australia" ~ 3,
@@ -41,26 +43,17 @@
     uniqv[which.max(tabulate(match(v, uniqv)))]
   }
   
-  x <- x %>% group_by(LGA_CODE16) %>% summarise(getmode(Remoteness))
+  x <- x %>% 
+        group_by(LGA_CODE16) %>% 
+        summarise(Remoteness_code = getmode(Remoteness)) %>%
+        mutate(Remoteness_area = case_when(Remoteness_code == 5 ~ "Very Remote Australia",
+                                           Remoteness_code == 4 ~ "Remote Australia",
+                                           Remoteness_code == 3 ~ "Outer Regional Australia",
+                                           Remoteness_code == 2 ~ "Inner Regional Australia",
+                                           Remoteness_code == 1 ~ "Major Cities of Australia"))
   
-  name <- "remoteness areas"
+  ggplot(data = x) +
+    geom_sf(aes(fill = Remoteness_code))
   
-  # Selection of regions to join
-  selection <- c("210193460",  "210203502",  "210253485",  "210183483",  "210183441",  "210183445",  "210183446",  "210183447",
-                 "210180001",  "210183442",  "210183448",  "210183457",  "210203481",  "210183458",  "210203486",  "210203475",
-                 "210203473",  "210203477",  "210213470",  "210213469",  "210203474",  "210203476",  "210203480",  "210203482",
-                 "210183468",  "210183456",  "210183454",  "210223493",  "210220002",  "210183449",  "210183452",  "210183453",
-                 "210183455",  "210213479",  "210213478",  "210223467",  "210183450",  "210223438",  "210223437",  "210223494",
-                 "210223496",  "210223498")
+  write.csv(x %>% st_set_geometry(NULL), "lga_remoteness.csv")
   
-  # Filter areas for just selected regions
-  output <- areas %>% filter(DZN_CODE16 %in% selection)
-  
-  # Join region
-  output <- st_union(output)
-  
-  # Plot it to check it looks right
-  plot(st_geometry(output))
-  
-  # Save it
-  st_write(output, paste0(name, ".shp"))
